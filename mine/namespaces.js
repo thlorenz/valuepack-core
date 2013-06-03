76467e708da132d94947c892f1ebc52c26cf68dd
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-02 21:28:55 -0400

    deps and initial core functionality

---
+++ mine/namespaces.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var npmusers    =  'npm-users'
+  , npmpackages =  'npm-packages'
+  , githubusers =  'github-users'
+  , githubrepos =  'github-repos';
+
+exports.npm = {
+
+    users     :  npmusers
+  , byGithub  :  'index-'    + npmusers    + '-byGithub'
+
+  , packages  :  npmpackages
+  , byOwner   :  'index-'    + npmpackages + '-byOwner'
+  , byKeyword :  'index-'    + npmpackages + '-byKeyword'
+};
+
+exports.github = {
+
+    users   :  githubusers
+
+  , repos   :  githubrepos
+  , byOwner :  'index-'    + githubrepos + '-byOwner'
+};
