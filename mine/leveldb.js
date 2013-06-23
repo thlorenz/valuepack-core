71ead92fbca1e3ce7ac5232bc9fc484c3b0573ca
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-23 11:24:14 -0400

    wrapping db in sublevel on open

--- mine/leveldb.js
+++ mine/leveldb.js
@@ -1,12 +1,18 @@
 'use strict';
 
 var level      =  require('levelup')
+  , sublevel   =  require('level-sublevel')
   , path       =  require('path')
   , dblocation =  process.env.VALUEPACK_MINE_DB || path.join(__dirname, '..', 'store/valuepack-mine.db');
 
 exports.location = dblocation;
 exports.destroy =  level.destroy.bind(level, dblocation);
-exports.open    =  level.bind(level, dblocation, { valueEncoding: 'json' });
+exports.open    =  function (cb) {
+  level(dblocation, { valueEncoding: 'json' }, function (err, db) {
+    if (err) return cb(err);
+    cb(null, sublevel(db));
+  });
+}
 
 exports.close = function done(err, db, cb) {
   if (err) { 
