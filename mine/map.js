2f59a830f51652289d703a67b982782bd6a14ab0
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-23 09:44:06 -0400

    adding db map to mine

---
+++ mine/map.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var nib = require('../').nib;
+var log = require('../util/log');
+var mapStream = require('map-stream');
+
+/**
+ * Maps the username to a stream of package infos for packages the user owns.
+ *
+ * @name userToPackageInfoStream
+ * @function
+ * @param npmPackages {Sublevel}
+ * @param byOwner {Sublevel}
+ * @param username {String} npm username
+ * @return {Stream} packageInfos from npmPackages sublevel
+ */
+exports.userToPackageInfoStream = function (npmPackages, byOwner, username) {
+
+  function packInfo (name, cb) {
+    npmPackages.get(name, function (err, pack) {
+      if (err) log.warn('github-logins', 'name', name, err);
+      if (!err && pack) cb(null, { user: username, pack: pack }); 
+      else cb();
+    });
+  }
+
+  return byOwner
+    .createReadStream({ start: username, end: username + nib, keys: false, values: true })
+    .pipe(mapStream(packInfo))
+}
+
