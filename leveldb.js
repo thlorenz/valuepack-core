8ca7676b9c2fa1dce523f30cb972cdb69b3f9992
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-31 13:37:53 -0400

    adding documentation to leveldb

--- leveldb.js
+++ leveldb.js
@@ -1,14 +1,33 @@
 'use strict';
 
-var level      =  require('levelup')
-  , sublevel   =  require('level-sublevel');
+var level    =  require('levelup')
+  , sublevel =  require('level-sublevel')
+  , log      =  require('./util/log')
+  ;
 
+/**
+ * Returns an API for a database at the given location.
+ * 
+ * @name exports
+ * @function
+ * @param dblocation {String} path to db
+ */
 module.exports = function (dblocation) {
   var leveldb = {};
 
+  // path to given location
   leveldb.location = dblocation;
+
+  // destroys the database at the given location by removing the database directory
   leveldb.destroy =  level.destroy.bind(level, dblocation);
 
+  /**
+   * Opens a database at the dblocation
+   * 
+   * @name open
+   * @function
+   * @param cb {Function} calls back with and error or the database with level-sublevel mixed in
+   */
   leveldb.open    =  function (cb) {
     level(dblocation, { valueEncoding: 'json' }, function (err, db) {
       if (err) return cb(err);
@@ -16,12 +35,21 @@ module.exports = function (dblocation) {
     });
   }
 
+  /**
+   * Closes the given database.
+   * 
+   * @name close
+   * @function
+   * @param err {Error} If not null it is logged to the console.
+   * @param db {LevelDB} database to close
+   * @param cb {Function}
+   */
   leveldb.close = function done(err, db, cb) {
     if (err) { 
-      console.error(err.stack);
-      console.error(err);
+      log.error('core-leveldb', err.stack);
+      log.error('core-leveldb', err);
     }
-    console.error('closing db');
+    log.info('core-leveldb', 'closing db');
     db && db.close(cb);
   };
 
